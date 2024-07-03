c1170bc51e7820fd31c209d419812cff2427b6cd
mamat5592 <spirit.mamat@gmail.com>
2024-07-03 23:35:22 +0330

    create and register base app 'app1'

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app1.apps.App1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
